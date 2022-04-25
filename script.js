32b2be7a8e62c82b9dad5af4a79d46d7b82d6b87
unknown <malufreirejau@yahoo.com.br>
2022-04-25 15:19:55 -0300

    adding game logic

--- script.js
+++ script.js
@@ -27,3 +27,66 @@ let lightColor = (element, number) => {
         element.classList.remove('selected');
     });
 }
+
+let checkOrder = () => {
+    for(let i in clickedOrder) {
+        if(clickedOrder[i] != order[i]) {
+            gameOver();
+            break;
+        }
+    }
+
+    if(clickedOrder.length == order.length) {
+        alert('Pontuação: ${score}\nVocê acertou! Iniciando próximo nível');
+        nextLevel();
+    }
+}
+
+let click = (color) => {
+    clickedOrder[clickedOrder.length] = color;
+    createColorElement(color).classList.add('selected');
+
+    setTimeout(() => {
+        createColorElement(color).classList.remove('selected');
+        checkOrder();
+    }, 250);
+}
+
+let createColorElement = (color) => {
+    if(color == 0) {
+        return green;
+    } else if(color == 1) {
+        return red;
+    } else if(color == 2) {
+        return yellow;
+    } else if (color == 3) {
+        return blue;
+    }
+}
+
+let nextLevel = () => {
+    score++;
+    shuffleOrder();
+}
+
+let gameOver = () => {
+    alert('Pontuação: ${score}!:\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo');
+    order = [];
+    clickedOrder = [];
+
+    playGame();
+}
+
+let playGame = () => {
+    alert('Bem vindo ao Gênesis! Iniciando novo jogo!')
+    score = 0;
+
+    nextLevel();
+}
+
+green.addEventListener('click', click(0));
+red.addEventListener('click', click(1));
+yellow.addEventListener('click', click(2));
+blue.addEventListener('click', click(3));
+
+playGame();
